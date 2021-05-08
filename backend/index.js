235c0116e2277e5b794df44046a1997f081ceb29
AryanKalwani <kalwani.aryan@gmail.com>
2021-05-09 03:24:17 +0530

    remove password

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const User = require("./models/User");
 const Authentication = require("./apiRoutes/Authentication");
 
 // WE NEED TO SET THIS UP AS ENV VARIABLE
-const client = new MongoClient("mongodb+srv://aryan:abcd1234@cluster0.39ko8.azure.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{useUnifiedTopology: true});
+const client = new MongoClient("mongodb+srv://aryan:<password>@cluster0.39ko8.azure.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{useUnifiedTopology: true});
 client.connect();
 
 app.post('/register', (req, res) => {
